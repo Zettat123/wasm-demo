11b21a53b2840a5355ad9f238f00f271f7ae0e1b
zettat123 <zettat123@gmail.com>
2019-04-03 14:14:12 +0800

    Refactor

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  entry: {
+    app: "./src/index.js"
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+    port: 3000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+}
